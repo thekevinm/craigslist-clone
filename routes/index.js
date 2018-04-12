29c58ed716dc1cd17f5689509490f515ec539b6a
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-04-12 15:53:41 -0700

    pulling titles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer')
+const conn = require('../lib/conn')
+
+
+// const upload = multer({})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home')
+  const sql = `
+	SELECT
+		title
+	FROM
+		categories
+  `
+
+  // conn.connect()
+  conn.query(sql, (err, results, fields) => {
+  	console.log(results)
+  	let data = {
+  		title: 'Home',
+  		categories: results
+  	}
+  	res.render('home', data)
+  })
+  // conn.end()
+
+  // res.render('home')
 });
 
 module.exports = router;
