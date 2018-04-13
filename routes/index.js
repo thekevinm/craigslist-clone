856739be8a56e538c0d9e799ed59b478654709fb
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-04-13 10:46:43 -0700

    added more styles and links to the correct category page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var multer = require('multer')
 const conn = require('../lib/conn')
 
@@ -35,6 +35,32 @@ router.get('/', function(req, res, next) {
   
 
   // res.render('home')
-});
+})
 
-module.exports = router;
+router.get('/:category', function(req, res, next) {
+	// console.log(req)
+	const sql = `
+	SELECT
+		*
+	FROM
+		listings
+	`
+	let data = {
+		title: 'Category',
+		category: req.params.category
+	}
+	conn.query(sql, (err, results, fields) => {
+  	data.categories = results.filter(result => result.parent_id === null)
+  	data.categories.map(cat => {
+  		let subcat = results.filter(result => {
+  			if(result.parent_id === cat.id){
+  				return result
+  			}
+  		})
+  		cat.subcat = subcat
+  	})
+})
+	res.render('category', data)
+})
+
+module.exports = router
